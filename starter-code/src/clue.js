26c28af288a1c213fe33dd03c049b6c2c89c857b
Dalina Weidinger <dalina.weidinger@gmail.com>
2019-10-15 11:50:10 +0200

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -1,62 +1,70 @@
 // Characters
-/*
-mrGreen
-first_name:   Jacob
-last_name:    Green
-color:        green
-description:  He has a lot of connections
-age:          45
-image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
-occupation:   Entrepreneur
 
-drOrchid
-frst_name:   Doctor
-lst_name:    Orchid
-color:        white
-description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
-age:          26
-image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
-ocupation:   Scientist
+const mrGreen = {
+  first_name: "Jacob",
+  last_name: "Green",
+  color: "green",
+  description: "He has a lot of connections",
+  age: 45,
+  image:
+    "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+  occupation: "Entrepreneur"
+};
 
-profPlum
-first_name:   Victor
-last_name:    Plum
-color:        purple
-description:  Billionare video game designer
-age:          22
-image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
-occupation:   Designer
+const drOrchid = {
+  frst_name: "Doctor",
+  lst_name: "Orchid",
+  color: "white",
+  description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+  age: 26,
+  image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+  ocupation: "Scientist"
+};
 
-missScarlet
-first_name:   Kasandra
-last_name:    Scarlet
-color:        red
-description:  She is an A-list movie star with a dark past
-age:          31
-image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
-occupation:   Actor
+const profPlum = {
+  first_name: "Victor",
+  last_name: "Plum",
+  color: "purple",
+  description: "Billionaire video game designer",
+  age: 22,
+  image: "https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg",
+  occupation: "Designer"
+};
 
-mrsPeacock
-first_name:   Eleanor
-last_name:    Peacock
-color:        blue
-description:  She is from a wealthy family and uses her status and money to earn popularity
-age:          36
-image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
-occupation:   Socialité
+const missScarlet = {
+  first_name: "Kasandra",
+  last_name: "Scarlet",
+  color: "red",
+  description: "She is an A-list movie star with a dark past",
+  age: 31,
+  image: "https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg",
+  occupation: "Actor"
+};
 
-mrMustard
-first_name:   Jack
-last_name:    Mustard
-color:        yellow
-description:  He is a former football player who tries to get by on his former glory
-age:          62
-image:        https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
-occupation:   Retired Football player
+const mrsPeacock = {
+  first_name: "Eleanor",
+  last_name: "Peacock",
+  color: "blue",
+  description:
+    "She is from a wealthy family and uses her status and money to earn popularity",
+  age: 36,
+  image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+  occupation: "Socialité"
+};
 
+const mrMustard = {
+  first_name: "Jack",
+  last_name: "Mustard",
+  color: "yellow",
+  description:
+    "He is a former football player who tries to get by on his former glory",
+  age: 62,
+  image: "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+  occupation: "Retired Football player"
+};
 // Weapons
 
-name: rope        ---   weight: 10
+/* name: rope        ---   weight: 10
 name: knife       ---   weight: 8
 name: candlestick ---   weight: 2
 name: dumbbell    ---   weight: 30
@@ -65,9 +73,55 @@ name: axe         ---   weight: 15
 name: bat         ---   weight: 13
 name: trophy      ---   weight: 25
 name: pistol      ---   weight: 20
+ */
+
+const rope = {
+  name: "rope",
+  weight: 10
+};
+
+const knife = {
+  name: "knife",
+  weight: 8
+};
+
+const candlestick = {
+  name: "candlestick",
+  weight: 2
+};
+
+const dumbbell = {
+  name: "dumbbell",
+  weight: 30
+};
+
+const poison = {
+  name: "poison",
+  weight: 2
+};
+
+const axe = {
+  name: "axe",
+  weight: 15
+};
+
+const bat = {
+  name: "bat",
+  weight: 13
+};
+
+const trophy = {
+  name: "trophy",
+  weight: 25
+};
+
+const pistol = {
+  name: "pistol",
+  weight: 20
+};
 
 // Rooms
-name: Dinning Room
+/* name: Dinning Room
 name: Conservatory
 name: Kitchen
 name: Study
@@ -83,12 +137,119 @@ name: Theater
 name: Guest House
 name: Patio
 */
+const diningRoom = {
+  name: "Dining Room"
+};
+
+const conservatory = {
+  name: "Conservatory"
+};
+
+const kitchen = {
+  name: "Kitchen"
+};
+
+const study = {
+  name: "Study"
+};
+
+const library = {
+  name: "Library"
+};
+
+const billiardRoom = {
+  name: "Billiard Room"
+};
+
+const lounge = {
+  name: "Lounge"
+};
+
+const ballroom = {
+  name: "Ballroom"
+};
+
+const hall = {
+  name: "Hall"
+};
+
+const livingRoom = {
+  name: "Living Room"
+};
+
+const observatory = {
+  name: "Observatory"
+};
+
+const theater = {
+  name: "Theater"
+};
+
+const guestHouse = {
+  name: "Guest House"
+};
+
+const patio = {
+  name: "Patio"
+};
 
 // Characters Collection
-var charactersArray = [];
 
-// Rooms' Collection
-var roomsArray = [];
+const charactersArray = [
+  mrGreen,
+  drOrchid,
+  drOrchid,
+  profPlum,
+  missScarlet,
+  mrsPeacock,
+  mrMustard
+];
+
+// Rooms Collection
+const roomsArray = [
+  rope,
+  knife,
+  candlestick,
+  dumbbell,
+  poison,
+  axe,
+  bat,
+  trophy,
+  pistol
+];
 
 // Weapons Collection
-var weaponsArray = [];
+const weaponsArray = [
+  diningRoom,
+  conservatory,
+  kitchen,
+  study,
+  library,
+  billiardRoom,
+  lounge,
+  ballroom,
+  hall,
+  livingRoom,
+  observatory,
+  theater,
+  guestHouse,
+  patio
+];
+
+function randomSelector(array) {
+  var rand = array[Math.floor(Math.random() * array.length)];
+  return rand;
+}
+let misteryEnvelope = [];
+function pickMistery() {
+  misteryEnvelope = [
+    randomSelector(charactersArray),
+    randomSelector(weaponsArray),
+    randomSelector(roomsArray)
+  ];
+  return misteryEnvelope;
+}
+
+function revealMistery(misteryEnvelope) {
+  return `${misteryEnvelope[0].first_name} ${misteryEnvelope[0].last_name} killed Mr.Boddy using the ${misteryEnvelope[1].name} in the ${misteryEnvelope[2].name}!!!!`;
+}
